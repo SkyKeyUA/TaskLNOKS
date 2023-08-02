08d96b06c10a56cebb532b3107ad40b08083057d
SkyKeyUA <veremiienkoov96@gmail.com>
2023-08-02 17:52:32 +0300

    server source

--- client/src/components/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-import React from 'react';
-import styles from './Header.module.scss';
-
-export const Header: React.FC = () => {
-  return <header className={styles.root}>Header</header>;
-};

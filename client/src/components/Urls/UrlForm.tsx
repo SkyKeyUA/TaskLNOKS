aaf4633c24b438b236b6e51b56603b6e17b74072
SkyKeyUA <veremiienkoov96@gmail.com>
2023-08-03 18:14:36 +0300

    Loader was created

--- client/src/components/Urls/UrlForm.tsx
+++ client/src/components/Urls/UrlForm.tsx
@@ -7,6 +7,7 @@ import axios from '@/api';
 import { useAppDispatch } from '@/hooks/redux';
 import { fetchShortUrls } from '@/redux/reducers/shortUrl/asyncActions';
 import { useShortUrlsSelector } from '@/redux/reducers/shortUrl/selectors';
+import { Loader } from '../Common/Loader';
 
 export const UrlForm: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -39,7 +40,7 @@ export const UrlForm: React.FC = () => {
       </button>
       <div style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>
         {statusUrls === 'loading' ? (
-          <div>Loading...</div>
+          <Loader />
         ) : shortUrls.length > 0 ? (
           `This is your last short Url: ${process.env.REACT_APP_API_URL}/shortUrl/${
             shortUrls[shortUrls.length - 1].shortUrl
